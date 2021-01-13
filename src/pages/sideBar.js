5f69be6f48dab03bae1888b9e5b8fa2011863d6a
SpIIIII <Shipin16@gmail.com>
2021-01-13 15:15:49 +0300

    first sidebar approximation

--- src/pages/sideBar.js
+++ src/pages/sideBar.js
@@ -1,61 +1,73 @@
 import React, { useRef, useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleRight, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { faBookmark } from '@fortawesome/free-regular-svg-icons'
+import { faBuffer } from '@fortawesome/free-brands-svg-icons'
 
-function SideBarPunct() {
-  const [selected, tagSelect] = useState(false);
-  const thisElRef = useRef(null);
-  const makeSelect = () => {
-    tagSelect(true);
-    thisElRef.current.className = "barTextSelect";
+
+
+
+function SideBarPunct(props) {
+  const [selected, togSelect] = useState(false)
+  const subPunktsRef = useRef(null);
+  const arrowRef = useRef(null)
+
+  const makeHovered = (x) => {
+      x.currentTarget.classList.add("barTextPreSelect");
   };
-  const makePreSelect = () => {
-    if (!selected) {
-      thisElRef.current.className = "barTextPreSelect";
+  const makeUnHovered = (x) => {
+      x.currentTarget.classList.remove("barTextPreSelect");
+  };
+
+  const makeSelected = (x) => {
+    if (selected) {
+      arrowRef.current.childNodes[0].classList.remove("sideBarArrowSelected")
+      subPunktsRef.current.style.display = "none"
+      x.currentTarget.classList.remove("barTextSelect")
     }
-  };
-  const makeUnSelect = () => {
-    if (!selected) {
-      thisElRef.current.className = "barText";
+    else{
+      arrowRef.current.childNodes[0].classList.add("sideBarArrowSelected")
+      subPunktsRef.current.style.display = "block"      
+      x.currentTarget.classList.add("barTextSelect")
     }
-  };
+    togSelect(!selected)
+  }
+  const arrowHower=(x)=>{
+    console.log(x.currentTarget.classList)
+    x.currentTarget.classList.add("sideBarArrowSelected")
+  }
 
   return (
-    <div
-      onClick={makeSelect}
-      onMouseEnter={makePreSelect}
-      onMouseLeave={makeUnSelect}
-      ref={thisElRef}
-    >
-      тест 1
-    </div>
+    <li className="sideBarPunkts">
+      <div className="sideBarPunkt dont_select_text" onClick={makeSelected} onMouseEnter={makeHovered}  onMouseLeave={makeUnHovered} >
+        {props.icon}
+        <span>{props.name}</span>
+        <div className="sideBarArrow" ref={arrowRef} >
+          <FontAwesomeIcon className="FAArrow" onMouseEnter={arrowHower} icon={faChevronRight} />
+        </div>
+      </div>
+      <ul className="sideBarSubPunkts dont_select_text" ref={subPunktsRef}>
+        <li className="sideBarSubPunkt" onMouseEnter={makeHovered}
+      onMouseLeave={makeUnHovered}>Шаблон 1</li>
+      </ul>
+    </li>
   );
 }
 
 function SideBar() {
+  const first = <span className="sideBarPunktIcon"><FontAwesomeIcon   icon={faBookmark} /></span>
+  const second =  <span className="sideBarPunktIcon"><FontAwesomeIcon   icon={faBuffer} /></span>
   return (
     <>
     <div className="fakeBar col-sm-2"></div>
-    <div className="sideBar sideBarFixed col-sm-2">
-      <ul className="barText header_style">
-        <li>
-          <SideBarPunct />
-        </li>
-        <li>
-          <SideBarPunct />
-        </li>
-        <li>
-          <SideBarPunct />
-        </li>
-        <li>
-          <SideBarPunct />
-        </li>
-        <li>
-          <SideBarPunct />
-        </li>
-        <li>
-          <SideBarPunct />
-        </li>
-      </ul>
-    </div>
+    <ul className=" barText header_style sideBarFixed col-sm-2 sideBar">
+          <SideBarPunct name={"Шаблоны"} icon={first} />      
+          <SideBarPunct name={"Другое"} icon={second}/>        
+          {/* <SideBarPunct />        
+          <SideBarPunct />        
+          <SideBarPunct />        
+          <SideBarPunct />         */}
+    </ul>
     </>
   );
 }
