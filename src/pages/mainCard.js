bff9c58e8efb31e3cb161c1431e0153f1daf05ce
SpIIIII <Shipin16@gmail.com>
2021-02-04 21:26:22 +0300

    start to transit leashes to mainC

--- src/pages/mainCard.js
+++ src/pages/mainCard.js
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 import Node from "./nodes/nodes";
 
 let ecxlNodes = ["Приветствие"];
-
+const leases = []
 
 function useForceUpdate() {
   const [value, setValue] = useState(0); // integer state
-  return () => setValue(value => ++value); // update the state to force render
+  return () => setValue((value) => ++value); // update the state to force render
 }
 
-function GraphWrapper(props){
+function GraphWrapper(props) {
   const rows = [];
   const forceUpdate = useForceUpdate();
 
@@ -18,13 +18,14 @@ function GraphWrapper(props){
     if (ecxlNodes.includes(n)) {
       ecxlNodes = ecxlNodes.filter((x) => x !== n);
     } else ecxlNodes.push(n);
-    props.graph.excludeNodes(ecxlNodes)
+    props.graph.excludeNodes(ecxlNodes);
   }
   for (let pair of props.graph.represente()) {
-    const node = pair[0]
-    const nods = [];
+    const node = pair[0];
+    const nodes = [];
+    
     for (let i of node) {
-      nods.push(
+      nodes.push(
         <Node
           title={i.tytle}
           type={i.type}
@@ -34,29 +35,55 @@ function GraphWrapper(props){
           upd={forceUpdate}
           parents={i.parents.map((x) => x.tytle)}
         />
-      );
+      )
+      leases.push([i.tytle, i.parents.map((x) => x.tytle)])
     }
-    // const selected = pair[1] ? " selectedRowModule" : ""
-    const classes = "row justify-content-center rowModule "
-    rows.push(
-      <div className={classes}>{nods}</div>
-    );
+    const classes = "row justify-content-center rowModule ";
+    rows.push(<div className={classes}>{nodes}</div>);
   }
-  return <>
-    <div className=" col-sm-10 mainCard" id="mainC">{rows}</div>;
-  </> 
-
+  return (
+    <>
+      <div className=" col-sm-10 mainCard" id="mainC">
+        {rows}
+      </div>
+    </>
+  );
 }
 
 function MainCard(props) {
-  const svg = document.createElementNS("http://www.w3.org/2000/svg", "path")    
-      svg.setAttributeNS(null, "d", `M 0 0 C 20 20,  100 100, 200 200`)
+  const paths = []
+  const findParentCenter = (titles = ["Приветствие"]) => {
+    // console.log(titles)
+    const nodes = titles.map((x) => document.getElementById(x));
+    const centers = nodes.map((x) => [
+      x.offsetLeft + x.offsetWidth / 2,
+      x.offsetTop + x.offsetHeight 
+    ]);
+
+    const relativeCenterCords = centers.map((x) => [x[0], x[1]]);
+    return relativeCenterCords;
+  };
+  const findChildCenter = (titles = "Приветствие") => {
+    const node = document.getElementById(titles)
+    const center = [
+      node.offsetLeft + node.offsetWidth / 2,
+      node.offsetTop 
+    ]
+    return center;
+  };
+  for(let pair of leases){
+    console.log("pair 0 ", pair[0])
+    x = findChildCenter(pair[0])
+    const relativeParentCenters = findParentCenter(pair[1]);
+    for (let parentCenter of relativeParentCenters) {
+      paths.push(<path d={ `M ${x} ${y} C ${x} ${y * 0.8}, ${parentCenter[0]} ${
+        parentCenter[1] / 0.8}, ${parentCenter[0]} ${parentCenter[1]}`}></path>)
+    }
+  }
   return (
     <>
-      <GraphWrapper graph={props.graph}/>
-      <svg className="node_leash">
-        <path d="M 0 0 C 20 20,  100 100, 200 200" fill="black"></path>
-      </svg>
+      <GraphWrapper graph={props.graph} />
+      <svg className="node_leash" id="svgLeash"></svg>
     </>
   )
 }
--- src/pages/nodes/nodes.js
+++ src/pages/nodes/nodes.js
@@ -1,5 +1,7 @@
 import React, { useRef, useState } from "react";
 
+
+
 function Node(props) {
   const thisElRef = useRef();
   const hoveredOn = (x) => {
@@ -14,37 +16,48 @@ function Node(props) {
   let sel = props.excl ? " nodeSelected" : " ";
   let classes = "col-md-auto node " + cl[props.type - 1] + sel;
 
+  const clearPaths = ()=>{
+    const svgLeash = document.getElementById("svgLeash");
+    while (svgLeash.firstChild){
+      svgLeash.removeChild(svgLeash.firstChild)
+    } 
+  }
+
   const findParentCenter = (titles = ["Приветствие"]) => {
     // console.log(titles)
     const nodes = titles.map((x) => document.getElementById(x));
-    console.log(nodes)
+    console.log(nodes);
     const centers = nodes.map((x) => [
       x.offsetLeft + x.offsetWidth / 2,
-      x.offsetTop + x.offsetHeight / 2
+      x.offsetTop + x.offsetHeight 
     ]);
 
-    const relativeCenterCords = centers.map((x) => [
-      x[0],
-      x[1]
-    ]);
+    const relativeCenterCords = centers.map((x) => [x[0], x[1]]);
     return relativeCenterCords;
   };
-  if(thisElRef.current){
-    const mainCard = document.getElementById("mainC")
-    const svgLeash = document.createElementNS("http://www.w3.org/2000/svg", "svg")
-    const pathLeash = document.createElementNS("http://www.w3.org/2000/svg", "path")
-    // console.log(props.parents)
+  if (thisElRef.current) {
+    const svgLeash = document.getElementById("svgLeash");
+    const pathLeash = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "path"
+    );
+    pathLeash.classList.add("path_leash");
+    var currentX =
+      thisElRef.current.offsetLeft + thisElRef.current.offsetWidth / 2;
+    var currentY =
+      thisElRef.current.offsetTop;
     const relativeParentCenters = findParentCenter(props.parents);
-    // console.log(relativeParentCenters);
-    for(let x of relativeParentCenters){
-      pathLeash.setAttributeNS(null, "d", `M 0 0 C 20 20,  ${x[0] / 2} ${x[1] / 2}, ${x[0]} ${x[1]}`)
-      svgLeash.appendChild(pathLeash)
-      mainCard.appendChild(svgLeash)
+    for (let x of relativeParentCenters) {
+      pathLeash.setAttributeNS(
+        null,
+        "d",
+        `M ${currentX} ${currentY} C ${currentX} ${currentY * 0.8}, ${x[0]} ${
+          x[1] / 0.8
+        }, ${x[0]} ${x[1]}`
+      );
+      svgLeash.appendChild(pathLeash);
     }
   }
-
-          
-
 
   return (
     <div
@@ -56,8 +69,9 @@ function Node(props) {
         props.exNode(x);
         props.upd();
         findParentCenter(props.parents);
+        clearPaths();
       }}
-      ref={thisElRef}
+      ref={(r) => (thisElRef.current = r)}
     >
       <span className=" header_style node_header_color">{props.title}</span>
       <div className="node_text_color text_style">{props.text}</div>
