9fbc297664e78a2d8c6223583b357aeb5548401e
SpIIIII <Shipin16@gmail.com>
2021-04-04 23:04:40 +0300

    added dots in nodes

--- src/pages/nodes/nodes.js
+++ src/pages/nodes/nodes.js
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from "react";
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faEllipsisV } from "@fortawesome/free-regular-svg-icons";
+// import { faEllipsisV } from "@fortawesome/free-brands-svg-icons";
+import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
 
 function Node(props) {
   const thisElRef = useRef();
@@ -16,17 +18,20 @@ function Node(props) {
   let sel = props.excl ? " nodeSelected" : " ";
   let classes = "col-md-auto node " + cl[props.type - 1] + sel;
 
-
   const findParentCenter = (titles = ["Приветствие"]) => {
     let nodes = titles.map((x) => document.getElementById(x));
-    nodes = nodes.fill(x=>x!==null)
+    nodes = nodes.fill((x) => x !== null);
     const centers = nodes.map((x) => [
       x.offsetLeft + x.offsetWidth / 2,
-      x.offsetTop + x.offsetHeight 
+      x.offsetTop + x.offsetHeight
     ]);
 
     const relativeCenterCords = centers.map((x) => [x[0], x[1]]);
     return relativeCenterCords;
+  };
+
+  const extraOpen = () => {
+    console.log("extra test");
   };
 
   return (
@@ -41,8 +46,13 @@ function Node(props) {
       }}
       ref={(r) => (thisElRef.current = r)}
     >
-      <span className=" header_style node_header_color">{props.title}</span>
-      <div className="node_text_color text_style">{props.text}</div>
+      <div className="header_style node_header_color">{props.title}</div>
+      <div className="extra" onMouseEnter={extraOpen}>
+        <FontAwesomeIcon className="FAextra" icon={faEllipsisV} />
+      </div>
+
+
+      {/* <div className="node_text_color text_style">{props.text}</div> */}
     </div>
   );
 }
